7f000a8d57f88e2c0ab207d6f510ea75490f70ff
jgrant12345 <justingrant99@me.com>
2024-07-21 21:29:59 -0700

    set up react router

--- adventure-manager/src/main.tsx
+++ adventure-manager/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import AdventureDashboard from './pages/adventure-dashboard.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AdventureDashboard/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ adventure-manager/src/pages/adventure-dashboard.tsx
@@ -0,0 +1,5 @@
+export default function AdventureDashboard(){
+    return (
+        <div>testing adventure dashboard</div>
+    )
+}
