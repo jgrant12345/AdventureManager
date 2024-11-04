b231d18212441d5bf7da220ca19facf65627219a
jgrant12345 <justingrant99@me.com>
2024-11-03 23:37:54 -0800

    update form

--- adventure-manager/src/components/LoginForm/LoginForm.tsx
+++ adventure-manager/src/components/LoginForm/LoginForm.tsx
@@ -1,22 +1,20 @@
 import { FormEvent } from "react";
 
 export default function LoginForm() {
-
-async function login(e ){
-  e.preventDefault();
-  console.log("testing")
-  const login = await fetch("/api/login", {
-    method: "post"
-  })
-  console.log("we made a post!")
-}
+  async function login(e: FormEvent) {
+    e.preventDefault();
+    console.log("testing");
+    const login = await fetch("/api/login", {
+      method: "post",
+    });
+    console.log("we made a post!");
+  }
 
   return (
     <div>
-
-        
+      <form>
         <button onClick={(e) => login(e)}>login</button>
-
+      </form>
     </div>
   );
 }
